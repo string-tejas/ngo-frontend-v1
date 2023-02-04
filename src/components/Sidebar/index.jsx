0fc752bc7527fe5feb9cb56af462f256b8f9d060
Tejas <jtjtejas05@gmail.com>
2023-02-05 00:23:37 +0530

    skeletal admin dashboard

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context";
+import Sidebar from "./SidebarElements";
+import { BiLogOut } from "react-icons/bi";
+
+const Index = () => {
+    const [selected, setSelected] = useState(window.location.pathname);
+    const navigate = useNavigate();
+
+    const auth = useAuth();
+
+    const handleSelect = (path) => {
+        setSelected(path);
+    };
+
+    return (
+        <Sidebar.Container>
+            <Sidebar.Header>Hello</Sidebar.Header>
+
+            <Sidebar.Separator />
+
+            <Sidebar.Navigation>
+                <Sidebar.Item
+                    label={"Volunteers"}
+                    selected={selected === "/dashboard"}
+                    to="/dashboard"
+                    onClick={() => handleSelect("/dashboard")}
+                />
+                <Sidebar.Item
+                    label={"Institutes"}
+                    to="/dashboard/institutes"
+                    selected={selected === "/dashboard/institutes"}
+                    onClick={() => handleSelect("/dashboard/institutes")}
+                />
+                <Sidebar.Item
+                    to="/dashboard/admins"
+                    label={"Admins"}
+                    selected={selected === "/dashboard/admins"}
+                    onClick={() => handleSelect("/dashboard/admins")}
+                />
+            </Sidebar.Navigation>
+
+            <Sidebar.Separator />
+
+            <Sidebar.Item
+                icon={<BiLogOut />}
+                label={"Logout"}
+                style={{
+                    marginTop: "auto",
+                }}
+                button
+            />
+        </Sidebar.Container>
+    );
+};
+
+export default Index;
--- src/page/DashboardAdmin.jsx
+++ src/page/DashboardAdmin.jsx
@@ -1,18 +1,33 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 import { useAuth } from "../context";
 
 const DashboardAdmin = () => {
     const { logOut } = useAuth();
     return (
-        <div>
-            <button
-                onClick={async () => {
-                    await logOut();
+        <>
+            <Navbar />
+            <div
+                style={{
+                    background: "#eee",
+                    width: "100%",
+                    height: "92vh",
+                    display: "grid",
+                    gridTemplateColumns: "1fr 4fr",
                 }}
             >
-                Logout
-            </button>
-        </div>
+                <div
+                    style={{
+                        background: "#ddd",
+                        width: "100%",
+                        height: "100%",
+                    }}
+                >
+                    <Sidebar />
+                </div>
+            </div>
+        </>
     );
 };
 
